f063fc8ac13d3fd56673950dcd3bd9478b7ae56e
Aaron Rosenberg <aarongrosenberg@gmail.com>
2012-11-09 14:08:06 -0500

    finding records in batches working

--- lib/batches_extension.rb
+++ lib/batches_extension.rb
@@ -1,7 +1,16 @@
 module BatchesExtension
-  def find_in_batches
+  def find_in_batches(opts={})
     return unless block_given?
+    num_offset = 0
 
-    yield(self.all)
+    relation = self.order(self.primary_key).limit(opts[:batch_size])
+    records = relation.all
+
+    while records.any?
+      yield(records)
+
+      num_offset += records.count
+      records = relation.offset(num_offset).all
+    end
   end
 end
--- test_app/spec/models/test_model_spec.rb
+++ test_app/spec/models/test_model_spec.rb
@@ -18,6 +18,30 @@ describe TestModel do
 
         records_found.should be_true
       end
+
+      it 'finds records in batches' do
+        num_records = TestModel.count
+        num_itterations = 0
+
+        (1..10).each do |batch_size|
+          expected_itterations = (num_records.to_f / batch_size).ceil
+
+          TestModel.find_in_batches(:batch_size => batch_size) do |records|
+            num_itterations += 1
+
+            expected_count = batch_size
+            if num_itterations == expected_itterations
+              last_count = num_records % batch_size
+              expected_count = last_count unless last_count.zero?
+            end
+
+            records.count.should == expected_count
+          end
+
+          num_itterations.should == expected_itterations
+          num_itterations = 0
+        end
+      end
     end
 
     describe '::find_each' do
