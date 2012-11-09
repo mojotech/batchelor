9b7a6ba319d6cbf29e45b4079b745a08cb6e3a4c
Aaron Rosenberg <aarongrosenberg@gmail.com>
2012-11-09 15:33:17 -0500

    cleanup using apply_finder_options and add warning as default batches does

--- lib/batches_extension.rb
+++ lib/batches_extension.rb
@@ -1,11 +1,17 @@
 module BatchesExtension
   def find_in_batches(opts={})
     return unless block_given?
+    batch_size = opts.delete(:batch_size) || 1000
     num_offset = 0
 
-    relation = self.order(self.primary_key).limit(opts[:batch_size])
-    relation = relation.select(opts[:select]) if opts[:select]
-    relation = relation.joins(opts[:joins]) if opts[:joins]
+    relation = self.reorder(self.primary_key).limit(batch_size)
+    unless opts.empty?
+      raise "You can't specify an order, it's forced to be the primary key" if opts[:order].present?
+      raise "You can't specify a limit, it's forced to be the batch_size"   if opts[:limit].present?
+      raise "You can't specify a start in this implementation"              if opts[:start].present?
+
+      relation = relation.apply_finder_options(opts)
+    end
 
     while (records = relation.offset(num_offset).all).any?
       yield(records)
